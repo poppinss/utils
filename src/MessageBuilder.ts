5147986ca1ce9138fcfea1b79c268a97c531d9f0
Harminder virk <virk.officials@gmail.com>
2020-04-12 09:32:33 +0530

    feat: add generic message builder

---
+++ src/MessageBuilder.ts
@@ -0,0 +1,96 @@
+/*
+* @adonisjs/encryption
+*
+* (c) Harminder Virk <virk@adonisjs.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+import ms from 'ms'
+import Bourne from '@hapi/bourne'
+
+/**
+ * Message builder exposes an API to JSON.stringify values by encoding purpose
+ * and expiryDate inside them. It returns a readable string, which is the
+ * output of `JSON.stringify`.
+ *
+ * Why use this over `JSON.stringify`?
+ *
+ * - It protects you from JSON poisioning
+ * - Allows encoding expiry dates to the message. It means, the message builer is
+ *   helpful, when you want to encode a message and pass it around, but also control
+ *   the TTL of the message
+ * - Allows encoding purpose. Again, useful for distribution.
+ */
+export class MessageBuilder {
+  private getExpiryDate (expiresIn?: string | number): undefined | Date {
+    if (!expiresIn) {
+      return undefined
+    }
+
+    const expiryMs = typeof (expiresIn) === 'string' ? ms(expiresIn) : expiresIn
+    if (expiryMs === undefined || expiryMs === null) {
+      throw new Error(`Invalid value for expiresIn "${expiresIn}"`)
+    }
+
+    return new Date(Date.now() + expiryMs)
+  }
+
+  /**
+   * Returns a boolean telling, if message has been expired or not
+   */
+  private isExpired (message: any) {
+    if (!message.expiryDate) {
+      return false
+    }
+
+    try {
+      const expiryDate = new Date(message.expiryDate)
+      return isNaN(expiryDate.getTime()) ||expiryDate < new Date()
+    } catch (error) {
+      return true
+    }
+  }
+
+  /**
+   * Builds a message by encoding expiry and purpose inside it
+   */
+  public build (message: any, expiresIn?: string | number, purpose?: string) {
+    const expiryDate = this.getExpiryDate(expiresIn)
+    return JSON.stringify({ message, purpose, expiryDate })
+  }
+
+  /**
+   * Verifies the message for expiry and purpose
+   */
+  public verify<T extends any> (message: any, purpose?: string): null | T {
+    try {
+      const parsed = Bourne.parse(message, {
+        protoAction: 'remove',
+      })
+
+      if (!parsed.message) {
+        return null
+      }
+
+      /**
+       * Ensure purposes are same
+       */
+      if (parsed.purpose !== purpose) {
+        return null
+      }
+
+      /**
+       * Ensure isn't expired
+       */
+      if (this.isExpired(parsed)) {
+        return null
+      }
+
+      return parsed.message
+    } catch (error) {
+      return null
+    }
+  }
+}
---
+++ test/message-builder.spec.ts
@@ -0,0 +1,76 @@
+/*
+* @adonisjs/encryption
+*
+* (c) Harminder Virk <virk@adonisjs.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+import test from 'japa'
+import { MessageBuilder } from '../src/MessageBuilder'
+
+test.group('MessageBuilder', () => {
+  test('build a number as a message', (assert) => {
+    const message = new MessageBuilder()
+    assert.equal(message.build(22), '{"message":22}')
+  })
+
+  test('build a string as a message', (assert) => {
+    const message = new MessageBuilder()
+    assert.equal(message.build('hello'), '{"message":"hello"}')
+  })
+
+  test('build a boolean as a message', (assert) => {
+    const message = new MessageBuilder()
+    assert.equal(message.build(true), '{"message":true}')
+  })
+
+  test('build a date as a message', (assert) => {
+    const message = new MessageBuilder()
+    const date = new Date()
+    assert.equal(message.build(date), `{"message":"${date.toISOString()}"}`)
+  })
+
+  test('return message value after verification', (assert) => {
+    const message = new MessageBuilder()
+    assert.equal(message.verify(message.build(22)), 22)
+  })
+
+  test('return null when purpose was defined during build, but not verify', (assert) => {
+    const message = new MessageBuilder()
+    assert.isNull(message.verify(message.build(22, undefined, 'login')))
+  })
+
+  test('return null when no purpose was defined during build', (assert) => {
+    const message = new MessageBuilder()
+    assert.isNull(message.verify(message.build(22), 'login'))
+  })
+
+  test('return null when purpose mismatch', (assert) => {
+    const message = new MessageBuilder()
+    assert.isNull(message.verify(message.build(22, undefined, 'register'), 'login'))
+  })
+
+  test('return null when message has been expired', (assert) => {
+    const message = new MessageBuilder()
+    assert.isNull(message.verify(message.build(22, -10)))
+  })
+
+  test('return null when expiredAt is not a valid date', (assert) => {
+    const message = new MessageBuilder()
+    const built = JSON.parse(message.build(22))
+    built.expiryDate = 'foo'
+    assert.isNull(message.verify(JSON.stringify(built)))
+  })
+
+  test('return null when message is malformed', (assert) => {
+    const message = new MessageBuilder()
+    assert.isNull(message.verify(JSON.stringify('foo')))
+  })
+
+  test('return message when not expired', (assert) => {
+    const message = new MessageBuilder()
+    assert.equal(message.verify(message.build(22, '10 hours')), 22)
+  })
+})
