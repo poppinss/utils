2cd2ed1814cbd4f6cefe39e58893df7802680364
Harminder virk <virk.officials@gmail.com>
2020-04-20 23:01:20 +0530

    feat: add ManagerConfigValidator

---
+++ src/ManagerConfigValidator.ts
@@ -0,0 +1,56 @@
+/*
+* @poppinss/utils
+*
+* (c) Harminder Virk <virk@adonisjs.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+import { Exception } from './Exception'
+
+/**
+ * A simple class to raise consistent exceptions for invalid config
+ * for driver based implementations.
+ */
+export class ManagerConfigValidator {
+  constructor (
+    private config: any,
+    private serviceName: string,
+    private configLocation: string,
+  ) {
+  }
+
+  /**
+   * Validates that the default key name is defined inside the config
+   * for a given module/service
+   */
+  public validateDefault (keyName: string) {
+    if (!this.config[keyName]) {
+      throw new Exception([
+        `Invalid "${this.serviceName}" config. Missing value for "${keyName}".`,
+        `Make sure set it inside "${this.configLocation}"`,
+      ].join(' '))
+    }
+  }
+
+  /**
+   * Validates that the list to ensure that is is defined and the default
+   * key name is also part of the list.
+   */
+  public validateList (listName: string, keyName: string) {
+    if (!this.config[listName]) {
+      throw new Exception([
+        `Invalid "${this.serviceName}" config. Missing value for "${listName}".`,
+        `Make sure set it inside "${this.configLocation}"`,
+      ].join(' '))
+    }
+
+    if (!this.config[listName][keyName]) {
+      throw new Exception([
+        `Invalid "${this.serviceName}" config. "${keyName}" is not defined inside "${listName}".`,
+        `Make sure set it inside "${this.configLocation}"`,
+      ].join(' '))
+    }
+  }
+}
