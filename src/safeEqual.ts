10bed15f8ae448c459733dd33993c8598686cbd1
Harminder virk <virk.officials@gmail.com>
2020-06-18 16:14:17 +0530

    improvement(safeEqual): strings are matched by converting them to the same length

--- src/safeEqual.ts
+++ src/safeEqual.ts
@@ -8,6 +8,7 @@
 */
 
 import { timingSafeEqual } from 'crypto'
+import alloc from 'buffer-alloc'
 
 type BufferSafeValue = ArrayBuffer
 | SharedArrayBuffer
@@ -19,6 +20,32 @@ type BufferSafeValue = ArrayBuffer
 /**
  * Generates a random string for a given size
  */
-export function safeEqual (value: BufferSafeValue, comparisonValue: BufferSafeValue) {
+export function safeEqual<T extends BufferSafeValue> (value: T, comparisonValue: T) {
+  if (typeof (value) === 'string' && typeof (comparisonValue) === 'string') {
+    /**
+     * The length of the main value to ensure that we compare equal strings
+     * against each other to get a constant time.
+     */
+    const expectedLength = Buffer.byteLength(value)
+
+    /**
+     * Value A
+     */
+    const valueBuffer = alloc(expectedLength, 0, 'utf-8')
+    valueBuffer.write(value)
+
+    /**
+     * Value B
+     */
+    const comparisonValueBuffer = alloc(expectedLength, 0, 'utf-8')
+    comparisonValueBuffer.write(comparisonValue)
+
+    /**
+     * Ensure values are same and also have same length
+     */
+    return timingSafeEqual(valueBuffer, comparisonValueBuffer)
+      && expectedLength === Buffer.byteLength(comparisonValue)
+  }
+
   return timingSafeEqual(Buffer.from(value), Buffer.from(comparisonValue))
 }
