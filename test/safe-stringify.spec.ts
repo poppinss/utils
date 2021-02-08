2c83eed8f0fbff3d9f796f9a9b731c82d97366e8
Harminder Virk <virk.officials@gmail.com>
2021-02-08 17:06:39 +0530

    test: copy tests from the PR

--- test/safe-stringify.spec.ts
+++ test/safe-stringify.spec.ts
@@ -38,4 +38,42 @@ test.group('Stringify', () => {
     a.a = a
     assert.deepEqual(safeStringify(a), '{"b":"10"}')
   })
+
+  test('stringifies object', (assert) => {
+    assert.deepEqual(safeStringify({ a: 18, b: 4 }), '{"a":18,"b":4}')
+  })
+
+  test('stringifies object (replacer)', (assert) => {
+    const replacer = (_, value) => {
+      return typeof value === 'number' ? value + 1 : value
+    }
+
+    assert.deepEqual(safeStringify({ a: 18, b: 4 }, replacer), '{"a":19,"b":5}')
+  })
+
+  test('stringifies object removing circular reference', (assert) => {
+    const o: any = { a: 18, b: 4 }
+    o.o = o
+    assert.deepEqual(safeStringify(o), '{"a":18,"b":4}')
+  })
+
+  test('stringifies object with bigint', (assert) => {
+    assert.deepEqual(safeStringify({ a: BigInt(18), b: 4 }), '{"a":"18","b":4}')
+  })
+
+  test('stringifies object with bigint (replacer returns bigint)', (assert) => {
+    const replacer = (_, value) => {
+      return typeof value === 'bigint' ? value + BigInt(1) : value
+    }
+
+    assert.deepEqual(safeStringify({ a: BigInt(18), b: 4 }, replacer), '{"a":"19","b":4}')
+  })
+
+  test('stringifies object with bigint (replacer handles bigint)', (assert) => {
+    const replacer = (_, value) => {
+      return typeof value === 'bigint' ? `${value.toString()}n` : value
+    }
+
+    assert.deepEqual(safeStringify({ a: BigInt(18), b: 4 }, replacer), '{"a":"18n","b":4}')
+  })
 })
