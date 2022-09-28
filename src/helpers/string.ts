745a3e3f4057346c38754d3100c7f8f36b29c3e4
Harminder Virk <virk.officials@gmail.com>
2022-09-28 15:31:55 +0530

    refactor: remove string.titleCase in favor of changeCase.titleCase

--- src/helpers/string.ts
+++ src/helpers/string.ts
@@ -15,40 +15,6 @@ import bytes, { BytesOptions } from 'bytes'
 
 export { default as toSlug } from 'slugify'
 
-const SMALL_WORDS =
-  /\b(?:an?d?|a[st]|because|but|by|en|for|i[fn]|neither|nor|o[fnr]|only|over|per|so|some|tha[tn]|the|to|up|upon|vs?\.?|versus|via|when|with|without|yet)\b/i
-const TOKENS = /[^\s:–—-]+|./g
-const WHITESPACE = /\s/
-const IS_MANUAL_CASE = /.(?=[A-Z]|\..)/
-const ALPHANUMERIC_PATTERN = /[A-Za-z0-9\u00C0-\u00FF]/
-
-/**
- * The method is a copy/paste from the "title-case" package. They have
- * a dependency on "tslib", which I don't want.
- */
-export function titleCase(input: string) {
-  let output = ''
-  let result: RegExpExecArray | null
-
-  while ((result = TOKENS.exec(input)) !== null) {
-    const { 0: token, index } = result
-
-    if (
-      !IS_MANUAL_CASE.test(token) &&
-      (!SMALL_WORDS.test(token) || index === 0 || index + token.length === input.length) &&
-      (input.charAt(index + token.length) !== ':' ||
-        WHITESPACE.test(input.charAt(index + token.length + 1)))
-    ) {
-      output += token.replace(ALPHANUMERIC_PATTERN, (char) => char.toUpperCase())
-      continue
-    }
-
-    output += token
-  }
-
-  return output
-}
-
 /**
  * Normalizes base64 string by removing special chars and padding
  */
