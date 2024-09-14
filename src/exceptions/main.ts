dc09554aa9fa0e29e253f65577efa2003792bf04
Harminder Virk <virk.officials@gmail.com>
2024-09-14 11:17:30 +0530

    feat: split exceptions and slash into its own imports
    Closes: #44

---
+++ src/exceptions/main.ts
@@ -0,0 +1,11 @@
+/*
+ * @poppinss/utils
+ *
+ * (c) Poppinss
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export * from './invalid_arguments_exception.js'
+export * from './runtime_exception.js'
