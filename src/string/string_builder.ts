8a4fe21a187aaae6b1eed6d6d69d8c018952f74f
Harminder Virk <virk.officials@gmail.com>
2025-05-23 14:41:18 +0530

    feat: remove named StringBuilder export
    BREAKING CHANGE

--- src/string/string_builder.ts
+++ src/string/string_builder.ts
@@ -7,7 +7,5 @@
  * file that was distributed with this source code.
  */
 
-import string from '@poppinss/string/builder'
-
-export * from '@poppinss/string/builder'
-export default string
+import StringBuilder from '@poppinss/string/builder'
+export default StringBuilder
